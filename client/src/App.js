b1d2f251f40a6391f48928d62e238036588805af
Matt Taylor <mattbtay@gmail.com>
2018-10-14 20:55:17 -0500

    init

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+          <p>{this.state.response}</p>
+      </div>
+    );
+
+}
+}
+
+export default App
