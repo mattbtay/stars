ba57da22973fc848908cf6a450d8a6877d892045
Matt Taylor <mattbtay@gmail.com>
2018-10-15 00:09:29 -0500

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,12 @@ class App extends React.Component {
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/stats');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -24,7 +24,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-          <p>{this.state.response}</p>
+          <p>games played {this.state.response.games}</p>
+          <p>record: {this.state.response.record}</p>
       </div>
     );
 
--- server.js
+++ server.js
@@ -1,12 +1,51 @@
 const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+app.get('/api/stats', (req, res) => {
+
+    const playoffsUrl = 'https://www.hockey-reference.com/friv/playoff_prob.cgi'
+    const teamUrl = 'https://www.hockey-reference.com/teams/DAL/2019.html';
+
+
+    request(teamUrl, function(error, response, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            // games played
+            var gamesPlayed = parseInt($("#team_stats td[data-stat='games']").html());
+            var gamesLeft = (82 - gamesPlayed).toString();
+
+            //wins
+            const wins = parseInt($("#team_stats td[data-stat='wins']").html());
+            //wins
+            const losses = parseInt($("#team_stats td[data-stat='losses']").html());
+            //wins
+            const lossesOt = parseInt($("#team_stats td[data-stat='losses_ot']").html());
+            const record = `${wins}-${losses}-${lossesOt}`;
+
+            var json = {
+                games: gamesLeft,
+                record:record,
+                chances: 'chances'
+
+            }
+
+            res.send(json);
+
+        } else {
+            console.log(error);
+        }
+    })
+
 });
+
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
